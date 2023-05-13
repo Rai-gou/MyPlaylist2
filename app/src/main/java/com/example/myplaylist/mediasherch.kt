94e964a46ec59b23d9628467d35795573f784485
Raigou <paladi.post@gmail.com>
2023-05-13 10:38:54 +0300

    two

---
+++ app/src/main/java/com/example/myplaylist/mediasherch.kt
@@ -0,0 +1,11 @@
+package com.example.myplaylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mediasherch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mediasherch)
+    }
+}
