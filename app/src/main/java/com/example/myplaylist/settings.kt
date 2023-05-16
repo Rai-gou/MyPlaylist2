38f9b0475c55632ded33d81b92cf4d02ba8c8f79
Raigou <paladi.post@gmail.com>
2023-05-16 22:19:53 +0300

    oneContextDone

--- app/src/main/java/com/example/myplaylist/settings.kt
+++ app/src/main/java/com/example/myplaylist/settings.kt
@@ -1,6 +1,7 @@
 package com.example.myplaylist
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -13,16 +14,41 @@ class settings : AppCompatActivity() {
         val actionBack = findViewById<ImageButton>(R.id.imageButton)
 
         actionBack.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+
             finish()
         }
 
         val sharing = findViewById<ImageButton>(R.id.imageButton2)
 
-        actionBack.setOnClickListener {
-
+        sharing.setOnClickListener {
+            val urlToShare = "https://practicum.yandex.ru/profile/android-developer/"
+            val sendText = Intent(Intent.ACTION_SEND)
+            sendText.type = urlToShare
+            startActivity(sendText)
         }
 
+        val shareButton = findViewById<ImageButton>(R.id.imageButton3)
+
+        shareButton.setOnClickListener {
+            val messageSubject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("raigou@yandex.ru"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, messageSubject)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+        val agreement = findViewById<ImageButton>(R.id.imageButton5)
+
+        agreement.setOnClickListener {
+            val urlToShare = "https://yandex.ru/legal/practicum_offer/"
+            val sendAgreement = Intent(Intent.ACTION_VIEW)
+            sendAgreement.data = Uri.parse(urlToShare)
+            startActivity(sendAgreement)
+        }
 
     }
 }
