47bc0d44912af443fc71d6c4b1f68493639d31ab
Raigou <paladi.post@gmail.com>
2023-05-17 20:14:44 +0300

    TextWatcherOne

--- app/src/main/java/com/example/myplaylist/search.kt
+++ app/src/main/java/com/example/myplaylist/search.kt
@@ -2,10 +2,16 @@ package com.example.myplaylist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class search : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val actionBack = findViewById<ImageButton>(R.id.imageButtonSearch)
+        actionBack.setOnClickListener {
+            finish()
+        }
     }
 }
